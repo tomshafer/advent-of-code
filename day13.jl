9f445ae70219e74774bd92cb7ab3e7dd9a8a167f
Tom Shafer <tom@tshafer.com>
2020-12-14 21:44:19 -0500

    Add days 13 and 14

---
+++ day13.jl
@@ -0,0 +1,56 @@
+
+using Printf
+
+function readinput(file::String; dict::Bool = false)
+    timestring, busstring = readlines(file)
+    buses = [parse(Float64, b) for b in split(busstring, ",") if b ≠ "x"]
+    parse(Float64, timestring), buses
+end
+
+function day13_puzzle01(file::String)
+    time, buses = readinput(file)
+    mods = [b - mod(time, b) for b in buses]
+    minidx = (mods .== minimum(mods))
+    solution = first(buses[minidx]) * first(mods[minidx])
+    println("Day 13, Puzzle 1: $(convert(Int, solution))")
+end
+
+function readinput2(file::String)
+    # Return the (zero-indexed) positions of non-x entries
+    _, busstring = readlines(file)
+    return [
+        (parse(Int, b), i - 1)
+        for (i, b) = enumerate(split(busstring, ","))
+        if b ≠ "x"
+    ]
+end
+
+function day13_puzzle02(file::String)
+    # For each bus, add multiples of the time so far to get the correct modulo
+    buses = reverse(readinput2(file))
+    t, increment = 0, 0
+
+    # Initialize with the first bus
+    a, m = first(buses)
+    t = a - m
+    increment = a
+
+    # For the remaining buses,
+    for (a, m) in buses[2:end]
+        # Lazy way to do this: Run until everything matches up
+        m_0 = m ÷ a
+        m_1 = mod(m, a)
+        for i in 0:(a-1)
+            test = t + i*increment
+            if (mod(test, a) == a - m_1) || (m_1 == 0 == mod(test, a))
+                t += i * increment
+                increment *= a
+                break
+            end
+        end
+    end
+    println("Day 13, Puzzle 2: $t")
+end
+
+day13_puzzle01("day13_input.txt")
+day13_puzzle02("day13_input.txt")
---
+++ day14.jl
@@ -0,0 +1,69 @@
+
+function bitstoint(bits::Array{Int})
+    sum(b * 2^(i - 1) for (i, b) in enumerate(reverse(bits)))
+end
+
+function day14_puzzle01(file::String)
+    mem = Dict{Int,Int}()
+    mask = repeat('X', 36)
+
+    for line in readlines(file)
+        if startswith(line, "mask")
+            global mask = replace(line, r"^mask = " => "")
+            global bitmask = [i for (i, m) = enumerate(mask) if m ≠ 'X']
+            global modified_vals = [parse(Int, x) for x in mask[bitmask]]
+            continue
+        end
+        position, value = map(
+            x -> parse(Int, x),
+            match(r"mem\[([0-9]+)\] = (\d+)", line).captures
+        )
+
+        bitvalue = reverse(digits(value, base = 2, pad = 36))
+        bitvalue[bitmask] = modified_vals
+        mem[position] = bitstoint(bitvalue)
+    end
+    println("Day 14, Puzzle 1: $(sum(values(mem)))")
+end
+
+function day14_puzzle02(file::String)
+    mem = Dict{Int,Int}()
+    mask = repeat('X', 36)
+
+    for line in readlines(file)
+        if startswith(line, "mask")
+            global mask = replace(line, r"^mask = " => "")
+            global bitmask_x = [i for (i, m) = enumerate(mask) if m == 'X']
+            global bitmask_o = [i for (i, m) = enumerate(mask) if m == '1']
+            global masked_vals = [parse(Int, x) for x in mask[bitmask_o]]
+            continue
+        end
+
+        position, value = map(
+            x -> parse(Int, x),
+            match(r"mem\[([0-9]+)\] = (\d+)", line).captures
+        )
+
+        # Get new, masked key
+        bitposition = reverse(digits(position, base = 2, pad = 36))
+        bitposition[bitmask_o] = masked_vals
+
+        # No combinatorics if no X's
+        if length(bitmask_x) == 0
+            mem[bitstoint(bitposition)] = value
+            continue
+        end
+
+        # Combinatorics
+        xprods = cat(Iterators.product([[0, 1] for x in bitmask_x]...)..., dims = 1)
+        for r in xprods
+            bitposition[bitmask_x] = [i for i in r]
+            mem[bitstoint(bitposition)] = value
+        end
+    end
+    println("Day 14, Puzzle 2: $(sum(values(mem)))")
+end
+
+
+day14_puzzle01("day14_input.txt")
+day14_puzzle02("day14_input.txt")
